c4306b628ee97cfd1204756abf4424f2bbbe0bbf
Nikita <lockshin1999@mail.ru>
2019-11-03 20:56:57 +0300

    new shaders

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <cstdio>
+#include <string>
+#include <fstream>
+#include <streambuf>
 #include <cstdlib>
 
 #include <glad/glad.h>
 #include "glad.c"
 #include <GLFW/glfw3.h>
+#include <sstream>
+#include <cmath>
 
 float vertices[] = {
-        0.5f, 0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f, 0.5f, 0.0f
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
 };
 unsigned int indices[] = {
         0, 1, 3,
@@ -19,17 +23,8 @@ unsigned int indices[] = {
 
 const unsigned int BUFFSIZE = 512;
 
-const char *fragmentShaderSource = "#version 330 core\n"
-                                   "out vec4 FragColor;\n"
-                                   "void main() {\n"
-                                   "   FragColor = vec4(0.5f, 0.2f, 0.0f, 1.0f);\n"
-                                   "}";
-
-const char *vertexShaderSource = "#version 330 core\n"
-                                 "layout(location = 0) in vec3 aPos;\n"
-                                 "void main() {\n"
-                                 "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                 "}";
+char *fragmentShaderSource;
+char *vertexShaderSource;
 
 void framebuffer_size_callback(GLFWwindow *w, int width, int height) {
     glViewport(0, 0, width, height);
@@ -42,6 +37,19 @@ void processInput(GLFWwindow* w) {
 }
 
 unsigned int initShaders() {
+
+    std::ifstream vertexdata("../vertexShaderSource.glsl");
+    std::stringstream buffer1;
+    buffer1 << vertexdata.rdbuf();
+    vertexShaderSource = new char [500];
+    strcpy(vertexShaderSource, buffer1.str().c_str());
+    buffer1.str(std::string());
+
+    std::ifstream fragmentdata("../fragmentShaderSource.glsl");
+    buffer1 << fragmentdata.rdbuf();
+    fragmentShaderSource = new char [500];
+    strcpy(fragmentShaderSource, buffer1.str().c_str());
+
     /* Set up and compile shaders */
     // vertex shader
     unsigned int vertexShader;
@@ -96,11 +104,14 @@ unsigned int bufferSetup() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3* sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     return VAO;
 }
@@ -134,20 +145,27 @@ int main() {
     }
 
     unsigned int VAO = bufferSetup();
-
     unsigned int shaderProgram = initShaders();
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // wireframe
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // wireframe
+    float timeValue, greenValue;
+    int vertexColorLocation;
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
         // rendering here
         glClearColor(0.2f, 0.3f, 0.3f, 0.1f);
         glClear(GL_COLOR_BUFFER_BIT);
+
         glUseProgram(shaderProgram);
+
+        timeValue = (float) glfwGetTime();
+        greenValue = ((float) sin(timeValue) / 2.0f) + 0.5f;
+        vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
